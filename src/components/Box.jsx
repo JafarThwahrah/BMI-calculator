d301b4e4ae9620455fd418fc191ca3bd30c6e811
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-11-01 22:34:10 +0200

    Go

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -10,15 +10,15 @@ const Box = (props) => {
         </h4>
       ) : props.data === "Overweight" ? (
         <h4 className="text-center">
-          Carefull you need to stop gaining calories otherwise you will be Obese
-          Soon
+          Carefull you need to stop gaining calories otherwise you will become
+          Obese
         </h4>
       ) : props.data === "Normalweight" ? (
-        <h4 className="text-center">You are Fine</h4>
-      ) : (
-        <h4 className="text-cent">
-          Eat more Food or you will be a skeleton soon
+        <h4 className="text-center">
+          Well Done, Your weight is in good proportion to your height
         </h4>
+      ) : (
+        <h4 className="text-cent">Eat more Food or you will be a skeleton</h4>
       )}
     </div>
   );
